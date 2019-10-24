375cb3b4081c8d7575d043a61fb93180bd62a0f9
marjolaMachnik <47745618+marjolaMachnik@users.noreply.github.com>
2019-10-24 11:07:35 +0200

    Add files via upload

--- src/test/java/pl/krakow/uek/pp5/creditcard/CreditCardTest.java
+++ src/test/java/pl/krakow/uek/pp5/creditcard/CreditCardTest.java
@@ -2,6 +2,7 @@ package pl.krakow.uek.pp5.creditcard;
 
         import org.junit.Assert;
         import org.junit.Test;
+        import pl.krakow.uek.pp5.creditcard.exceptions.CreditBelowMinimumException;
 
         import java.math.BigDecimal;
 
@@ -17,5 +18,16 @@ public class CreditCardTest {
         card.assignLimit(BigDecimal.valueOf(NEW_CREDIT_LIMIT));
         //Assert // Then // Expect
         Assert.assertTrue(card.getLimit().equals(BigDecimal.valueOf(NEW_CREDIT_LIMIT)));
+    }
+
+    @Test
+    public void itVerifyMinimumCreditValue()
+    {
+        try {
+            card.assign(50);
+            Assert.fail("Below minimum exception will be thrown");
+        }catch (CreditBelowMinimumException e){
+            Assert.assertTrue(condition true);
+        }
     }
 }
